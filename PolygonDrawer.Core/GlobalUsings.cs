fcb19a47dc1973b14364d2544801d16ed1d68010
xxxDKGxxx <dominikzielinski678@gmail.com>
2025-10-29 23:43:00 +0100

    Remove unused global using

--- PolygonDrawer.Core/GlobalUsings.cs
+++ PolygonDrawer.Core/GlobalUsings.cs
@@ -2,5 +2,4 @@
 global using PolygonDrawer.Core.Edges;
 global using PolygonDrawer.Core.Edges.EdgeTypes;
 global using PolygonDrawer.Core.Rendering;
-global using System.Diagnostics;
 global using System.Numerics;
